1f37f91cdc433da1da34e9ac30d4990c28621714
Fernando Melo <94711394+Fernando12filho@users.noreply.github.com>
2024-09-19 10:24:13 -0700

    adicao de comentarios

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, redirect, request
 from flask_openapi3 import OpenAPI
-import db
+import db #importa o banco de dados
 from flask_cors import CORS
 
 #comentario teste
@@ -8,19 +8,24 @@ app = OpenAPI(__name__)
 app.config['Database'] = 'tasks.db'
 CORS(app)
 
+#inicia banco de dados
+db.init_app(app)
+
 @app.route('/')
 def index():
     return redirect('/openapi')
 
+
+#Rota para pegar tasks do banco de dados e passar para o front 
 @app.route('/tasks', methods=['GET'])
 def select_tasks():
     print("In route tasks")
-    db_conn = db.get_db()  # Get the database connection
+    db_conn = db.get_db() 
     tasks = db_conn.execute(
         'SELECT id, title, descricao, completed FROM tasks'
     ).fetchall()
 
-    # Convert tasks into a list of dictionaries to return as JSON
+    #transformar em uma lista de dicionarios para passar para JSON
     tasks_list = []
     for task in tasks:
         tasks_list.append({
@@ -32,12 +37,14 @@ def select_tasks():
 
     return jsonify(tasks_list)
 
+#Rota para inserir um terefa no banco de dados
 @app.route('/insert', methods=['POST'])
 def insert_task():
     title = request.form['title']
     description = request.form['description']
     completed = False
 
+    #Connexao com o banco de dados, sempre mesmo padrao
     db_conn = db.get_db()
     db_conn.execute(
         'INSERT INTO tasks (title, descricao, completed) VALUES (?, ?, ?)',
@@ -45,6 +52,7 @@ def insert_task():
     )
     db_conn.commit()
 
+#Deletar tarefas ja feitas, deixas tarefas que ainda nao foram resolvidas
 @app.route('/end-day', methods=['POST'])
 def end_day():
     db_conn = db.get_db()
@@ -54,6 +62,7 @@ def end_day():
     db_conn.commit()
     return {'message': 'Completed tasks deleted'}, 200
 
+#Atualiza tarefas feitas
 @app.route('/update/<int:task_id>', methods=['POST'])
 def update_task(task_id):
     data = request.json
@@ -67,5 +76,3 @@ def update_task(task_id):
     db_conn.commit()  # Commit the transaction
 
     return jsonify({"message": "Task Updated Successfully"})
-
-db.init_app(app)
--- db.py
+++ db.py
@@ -2,6 +2,7 @@ import sqlite3
 import click 
 from flask import current_app, g
 
+#Criando conexao com o banco de dados
 def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
@@ -11,21 +12,26 @@ def get_db():
         g.db.row_factory = sqlite3.Row
     return g.db
 
+#inicia comandos SQL, do arquivo 'schema.sql'
 def init_db():
     db = get_db()
 
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
 
+#cria um comando no console para iniciar o database
+# > flask --app app init-db
 @click.command('init-db')
 def init_db_command():
     init_db()
     click.echo('Database Initialized')
 
+#registra banco de dados no app.py
 def init_app(app):
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
 
+#fecha chamada com o banco de dados
 def close_db(e=None):
     db = g.pop('db', None)
 
